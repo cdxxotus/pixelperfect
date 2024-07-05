e1e7c9b500761d80d48e8ab451ee8d696c13db18
Daniel Febrero <febrero.daniel@gmail.com>
2024-07-05 10:43:12 +0200

    uncompilation*

--- pplang/compilers/python/logical_doors/main.py
+++ pplang/compilers/python/logical_doors/main.py
@@ -3,20 +3,20 @@ doors = {
     """+""": {},
     """*""": {},
     """€""": {
-        "compilation_state_update": {
+        "uncompilation_state_update": {
             "is_escaped": True,
         }
     },
     """$""": {},
     """@""": {},
     """{""": {
-        "compilation_state_update": {
+        "uncompilation_state_update": {
             "schema_object_key_idx": 0,
         }
     },
     """}""": {},
     """[""": {
-        "compilation_state_update": {
+        "uncompilation_state_update": {
             "schema_array_idx": 0,
         }
     },
