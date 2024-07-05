7fd54f3cec51f80d3c71c8c50030b24527942ff8
Daniel Febrero <febrero.daniel@gmail.com>
2024-07-05 21:34:07 +0200

    pplang: operators: magic: make magic open to moldu by letting them learn and apply spells

--- pplang/compilers/python/operators/magic.py
+++ pplang/compilers/python/operators/magic.py
@@ -1,9 +1,20 @@
-import uuid
 import inspect
 
 def make(session_state):
-    reserved_functions = {}
+    spells_registry = {}
 
+    def register_spell(name, func):
+        spells_registry[name] = func
+
+    def apply_spell(spell_name):
+        def decorator(func):
+            def wrapper(*args, **kwargs):
+                if spell_name in spells_registry:
+                    return spells_registry[spell_name](func, *args, **kwargs)
+                return func(*args, **kwargs)
+            return wrapper
+        return decorator
+    
     def magify(magic_number=None):
         def decorator(func):
             def wrapper(*args, **kwargs):
@@ -25,7 +36,6 @@ def make(session_state):
                         return 42
                     elif behavior == "reserved":
                         stack = inspect.stack()
-                        caller_frame = stack[1]
                         caller_parent_frame = stack[2] if len(stack) > 2 else None
                         func_parent_frame = stack[3] if len(stack) > 3 else None
                         
@@ -33,6 +43,12 @@ def make(session_state):
                             pass
                         else:
                             return 42
+                    elif behavior=="learn_spell":
+                        register_spell(behavior_args[0],behavior_args[1])
+                        pass
+                    elif behavior in spells_registry.keys():
+                        apply_spell(behavior_args)
+                        pass
                     return None
                 return func(*args, **kwargs)
             return wrapped_func
