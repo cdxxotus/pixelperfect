29409a47b702d957c717aaba0075516ee8523786
Daniel Febrero <febrero.daniel@gmail.com>
2024-07-05 11:19:21 +0200

    iterating

--- pplang/compilers/python/logical_doors/main.py
+++ pplang/compilers/python/logical_doors/main.py
@@ -1,3 +1,67 @@
+
+def make_new_compilation():
+    state = {
+        "uncompiled_string": "",
+        "live_memory": {}
+    }
+    
+    def get_pointer_name_at_pos(pos):
+        # trytogetit from memory
+        # else from pointers
+        pointer_name=""
+        state["live_memory"][pos]=pointer_name
+    
+    return {
+        
+        "doors": {
+            """any""": {
+                "uncompilation_state_update": {
+                    "is_escaped": False,
+                }
+            },
+            """pointer_pos""": {
+                "uncompilation_state_update": {},
+                "uncompilation_memory_get": {"pointer_name": get_pointer_name_at_pos},
+                "uncompilation_schema_build": {"append": f""},
+            },
+            """$""": {},
+            """+""": {},
+            """*""": {},
+            """€""": {
+                "uncompilation_state_update": {
+                    "is_escaped": True,
+                }
+            },
+            """$""": {},
+            """@""": {},
+            """{""": {
+                "uncompilation_state_update": {
+                    "schema_object_key_idx": 0,
+                }
+            },
+            """}""": {},
+            """[""": {
+                "uncompilation_state_update": {
+                    "schema_tupple_idx": 0,
+                }
+            },
+            """]""": {},
+            """(""": {
+                "uncompilation_state_update": {
+                    "in_parenthesis": True,
+                }
+            },
+            """)""": {
+                "uncompilation_state_update": {
+                    "in_parenthesis": False,
+                }
+            },
+            """,""": {},
+        },
+    }
+    
+uncompilations=[]
+
 doors = {
     """any""": {
         "uncompilation_state_update": {
@@ -5,8 +69,12 @@ doors = {
         }
     },
     """pointer_pos""": {
-        "uncompilation_state_update": {
-            "is_escaped": False,
+        "uncompilation_state_update": {},
+        "uncompilation_memory_get": {
+            "pointer_name": get_pointer_name_at_pos
+        },
+        "uncompilation_schema_build":{
+            "append": f""
         }
     },
     """$""": {},
@@ -32,9 +100,14 @@ doors = {
     },
     """]""": {},
     """(""": {
-
+        "uncompilation_state_update": {
+            "in_parenthesis": True,
+        }
     },
-    """)""": {},
+    """)""": {
+        "uncompilation_state_update": {
+            "in_parenthesis": False,
+        }
+    },
     """,""": {},
 }
-
